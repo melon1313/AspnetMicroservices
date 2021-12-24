f7550b82707ad54ff546ba07daeab50dc2518322
melon1313 <10212130@gm.nttu.edu.tw>
2021-12-24 16:37:37 +0800

    [Ordering] add entity and valueObject

---
+++ src/Services/Ordering/Ordering.Domain/Common/ValueObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class ValueObject
+    {
+        protected static bool EqualOperator(ValueObject left, ValueObject right)
+        {
+            if(left is null ^ right is null) return false;
+           
+            return left?.Equals(right) != false; //邏輯怪怪的
+
+        }
+
+        protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+        {
+            return !(EqualOperator(left, right));
+        }
+
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || obj.GetType() != GetType()) return false;
+
+            var other = (ValueObject)obj;
+
+            return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                    .Select(x => x != null ? x.GetHashCode() : 0)
+                    .Aggregate((x, y) => x ^ y); //byte xor byte
+        }
+    }
+}
+ 
